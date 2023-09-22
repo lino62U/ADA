1866e544f5ca6bd0655204798f204e0f70bc873e
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2023-09-21 23:02:52 -0500

    Update dimensionalityCurse.cpp

--- LAB_01/dimensionalityCurse.cpp
+++ LAB_01/dimensionalityCurse.cpp
@@ -1,24 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <cmath>
+#include <fstream>
 
-#include "iostream"
-#include "vector"
-#include "random"
-#include "cmath"
-#include "fstream"
 using namespace std;
 
+// Clase para representar un punto en n dimensiones
+class Point {
+public:
+    vector<double> dimensions;
+
+    Point(int dimension) {
+        dimensions.resize(dimension);
+    }
+};
+
 // Función para calcular la distancia euclidiana entre dos puntos
-double euclideanDistance(const vector<double>& p1, const vector<double>& p2) {
+double euclideanDistance(const Point& p1, const Point& p2) {
     double sum = 0.0;
-    for (size_t i = 0; i < p1.size(); ++i) {
-        double diff = p1[i] - p2[i];
+    for (size_t i = 0; i < p1.dimensions.size(); ++i) {
+        double diff = p1.dimensions[i] - p2.dimensions[i];
         sum += diff * diff;
     }
     return sqrt(sum);
 }
 
-
 // Función para calcular las distancias entre todos los pares de puntos
-vector<double> calculateDistances(const vector<vector<double>>& points) {
+vector<double> calculateDistances(const vector<Point>& points) {
     vector<double> distances;
 
     for (size_t i = 0; i < points.size(); ++i) {
@@ -32,45 +41,40 @@ vector<double> calculateDistances(const vector<vector<double>>& points) {
 }
 
 // Función para generar puntos aleatorios en una dimensión d
-vector<vector<double>> generateRandomPoints(int numPoints, int dimension)
-{
-    vector<vector<double>> points(numPoints, vector<double>(dimension));
+vector<Point> generateRandomPoints(int numPoints, int dimension) {
+    vector<Point> points(numPoints, Point(dimension));
     random_device rd;
     mt19937 gen(rd());
     uniform_real_distribution<> dis(0.0, 1.0);
 
     for (int i = 0; i < numPoints; ++i) {
         for (int j = 0; j < dimension; ++j) {
-            points[i][j] = dis(gen);
+            points[i].dimensions[j] = dis(gen);
         }
     }
 
     return points;
 }
 
-
 // Función para generar un archivo de texto con las distancias
-void saveDistancesToFile(const std::vector<double>& distances, int dimension) {
-    std::string filename = "distances_d" + std::to_string(dimension) + ".txt";
-    std::ofstream outFile(filename);
+void saveDistancesToFile(const vector<double>& distances, int dimension) {
+    string filename = "distances_d" + to_string(dimension) + ".txt";
+    ofstream outFile(filename);
 
     for (double distance : distances) {
-        outFile << distance << std::endl;
+        outFile << distance << endl;
     }
-
     outFile.close();
 }
 
-int main()
-{
+int main() {
+    //vector<int> dimensions = {2};
     vector<int> dimensions = {10, 50, 100, 500, 1000, 2000, 5000};
-    //vector<int> dimensions = {10};
     for (int d : dimensions) {
-        vector<vector<double>> points = generateRandomPoints(100, d);
+        vector<Point> points = generateRandomPoints(100, d);
 
         vector<double> distances = calculateDistances(points);
-        cout<<"d: "<<distances.size()<<endl;
-        //generateHistogram(distances, 50, d);
+        cout << "d: " << distances.size() << endl;
         saveDistancesToFile(distances, d);
     }
 
